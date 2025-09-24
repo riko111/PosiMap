e2651661077778227eb61af9376fbde9b882533e
riko <riko.lunch@gmail.com>
2025-09-24 17:12:43 +0900

    やりなおし

--- shared/src/commonMain/kotlin/com/isoffice/posimap/gateway/ShareGateway.kt
+++ shared/src/commonMain/kotlin/com/isoffice/posimap/gateway/ShareGateway.kt
@@ -1,13 +1,6 @@
 package com.isoffice.posimap.gateway
 
-expect class ShareGateway(
-    context: ShareGatewayContext? = null,
-) {
-    fun share(
-        bytes: ByteArray,
-        filename: String,
-        mime: String = "application/vnd.posimap+json",
-    )
-}
 
-expect class ShareGatewayContext
+interface ShareGateway {
+    fun share(bytes: ByteArray, filename: String, mime: String = "application/vnd.posimap+json")
+}
